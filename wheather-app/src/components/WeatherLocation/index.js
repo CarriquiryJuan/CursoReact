4d6389a987cf441681e0d073182047f8c99e9e0d
Juan Carriquiry <jcarriquiry@ica.com.uy>
2020-01-24 12:04:45 -0300

    avances

--- wheather-app/src/components/WeatherLocation/index.js
+++ wheather-app/src/components/WeatherLocation/index.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React,{Component} from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
+import './styles.css';
+import {
+  SUN, WINDY  /*,CLOUD, CLOUDY, RAIN, SNOW, WINDY */
+} from "./../../constants/weathers";
 //import PropTypes from "prop-types";
+const data={
+  temperature: 21,
+  weatherState:SUN,
+  humidity:50,
+  wind:'60 m/s',
+}
+const data2={
+  temperature: 10,
+  weatherState:WINDY,
+  humidity:80,
+  wind:'60 m/s',
+}
+class WeatherLocation extends Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      city:"Buenos Aires",
+      data:data,
+    };
 
-const WeatherLocation = () => (
-  <div>
-    <Location city={"eee"}></Location>
-    <WeatherData></WeatherData>
-  </div>
-);
+  }
+
+  handleUpdateClick=()=>{
+    console.log("actualizado");
+    this.setState( {
+      city: "Buenos Aires!",
+      data: data2,})
+  }
+  
+  render(){
+    const{city,data}=this.state; 
+    return (
+    <div className="weatherLocationCont">
+      <Location city={city}></Location>
+      <WeatherData data={data}></WeatherData>
+      <button onClick={this.handleUpdateClick}>Actualizar</button>
+    </div>);
+  }
+};
 
 //WeatherLocation.propTypes = {};
 export default WeatherLocation;
